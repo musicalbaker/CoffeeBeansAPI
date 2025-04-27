f8a4a4b1e45377751eaed308e432da8c41df2c8f
Calvin Laptop <kipling3@hotmail.co.uk>
2025-04-27 21:16:54 +0100

    [Files] Add test project.

---
+++ CoffeeBeansApi.Tests/TestMethods.cs
@@ -0,0 +1,37 @@
+﻿namespace CoffeeBeansApi.Tests
+{
+    [TestClass]
+    public sealed class TestMethods
+    {
+
+        [TestMethod]
+        public void GetProducts()
+        {
+            
+        }
+
+        [TestMethod]
+        public void GetSingleProduct()
+        {
+
+        }
+
+        [TestMethod]
+        public void AddProduct()
+        {
+
+        }
+
+        [TestMethod]
+        public void DeleteProduct()
+        {
+
+        }
+
+        [TestMethod]
+        public void AlterProduct()
+        {
+
+        }
+    }
+}
