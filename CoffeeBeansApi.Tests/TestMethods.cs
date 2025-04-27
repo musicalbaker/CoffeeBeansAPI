01994640a0e00ca776a9dca8b6c243f8c90ab165
Calvin Laptop <kipling3@hotmail.co.uk>
2025-04-27 21:23:55 +0100

    [code] Add start of test methods.

--- CoffeeBeansApi.Tests/TestMethods.cs
+++ CoffeeBeansApi.Tests/TestMethods.cs
@@ -1,19 +1,33 @@
-﻿namespace CoffeeBeansApi.Tests
+﻿using System.Net;
+
+namespace CoffeeBeansApi.Tests
 {
     [TestClass]
     public sealed class TestMethods
     {
+        private HttpClient _client;
+        public TestMethods()
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:7294/api/coffee")
+            };
+        }
 
         [TestMethod]
         public void GetProducts()
         {
-            
+            var response = _client.GetAsync("").GetAwaiter().GetResult();
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         }
 
         [TestMethod]
         public void GetSingleProduct()
         {
+            var response = _client.GetAsync("66a374593a88b14d9fff0e2e").GetAwaiter().GetResult();
 
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         }
 
         [TestMethod]
